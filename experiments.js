84515bca0063efe78a60fe8b9aef188ef42b770d
Baykam Say <47826352+baykamsay@users.noreply.github.com>
2020-03-15 17:39:15 +0300

    Initial commit

---
+++ experiments.js
@@ -0,0 +1,18 @@
+// // Part 1
+// const monthlyRent = 500;
+// const yearlyRent = monthlyRent * 12;
+// console.log(monthlyRent, yearlyRent);
+
+// // Part 2
+// const firstName = 'Baykam'; // Same as "Baykam Say" and `Baykam Say`
+// const lastName = 'Say';
+// const sentence = `Hello ${firstName} ${lastName}! How are you?`;
+// console.log(sentence)
+
+// // Part 3
+// const isTheSkyBlue = true; // lets are undefined at first
+// console.log(isTheSkyBlue);
+
+// Part 4
+const num = 5;
+console.log(num);
