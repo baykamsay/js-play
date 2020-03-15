f77b9847f5806440b024d1b0e267fe6234ebb7d8
Baykam Say <47826352+baykamsay@users.noreply.github.com>
2020-03-15 19:19:46 +0300

    Update experiments.js

--- experiments.js
+++ experiments.js
@@ -13,6 +13,56 @@
 // const isTheSkyBlue = true; // lets are undefined at first
 // console.log(isTheSkyBlue);
 
-// Part 4
-const num = 5;
-console.log(num);
+// // Part 4
+// const num = 5;
+// console.log(num);
+
+// // Part 5
+// const isSkyBlue = false;
+
+// let greeting;
+
+// if (isSkyBlue) {
+//     greeting = "It must be nice weather";
+// } else {
+//     greeting = "It must be bad weather";
+// }
+
+// console.log(greeting);
+
+// // Part 6
+// if (2 + 2 === 4) { // == works too but coercion might cause errors
+//     console.log("Yeey!");
+// } else {
+//     console.log("Oh no...");
+// }
+
+// Part 7
+// <= comparator
+
+// // Part 8
+// let friendsAtYourParty = 0;
+
+// while (friendsAtYourParty < 10) {
+//     // console.log(friendsAtYourParty);
+//     friendsAtYourParty = friendsAtYourParty + 1;
+// }
+
+// console.log(friendsAtYourParty);
+
+// Part 9
+// Comments: // and /* */
+
+// Part 10
+// Add +=
+// Increment++
+
+// Part 10
+let friendsAtYourParty = 0;
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    friendsAtYourParty++;
+}
+
+console.log(friendsAtYourParty);
